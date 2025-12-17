d8d6a2d25d17ba90387fe7ea3a9da2a453ff9bea
David V. Kimball <david@davidvkimball.com>
2025-12-17 10:30:04 -0800

    Fixed eslint config.

--- main.ts
+++ main.ts
@@ -66,7 +66,8 @@ export default class MyPlugin extends Plugin {
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		const settingTab = new SampleSettingTab(this.app, this);
+		this.addSettingTab(settingTab);
 
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
